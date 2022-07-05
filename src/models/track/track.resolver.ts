2429a3dce16e85124dd48f7e9107873e2cc1877f
Oleg Vaskevich <oleg.vaskevich.it@gmail.com>
2022-07-05 22:52:27 +0300

    feat: implement track queries & mutations

--- src/models/track/track.resolver.ts
+++ src/models/track/track.resolver.ts
@@ -10,7 +10,28 @@ const trackResolver = {
       return res;
     },
   },
-  Mutation: {},
+  Mutation: {
+    async createTrack(_parent: any, { track }: any, { dataSources }: any) {
+      const { bandsIds, genresIds, albumId, artistsIds, ...rest } = await dataSources.trackAPI.postOne(track);
+      const album = await dataSources.albumAPI.getOne(albumId);
+      const artists = await Promise.all(artistsIds.map((artistId: any) => dataSources.artistAPI.getOne(artistId)));
+      const bands = await Promise.all(bandsIds.map((bandId: any) => dataSources.bandAPI.getOne(bandId)));
+      const genres = await Promise.all(genresIds.map((genreId: any) => dataSources.genreAPI.getOne(genreId)));
+      return { bands, genres, album, artists, ...rest };
+    },
+    async updateTrack(_parent: any, { track }: any, { dataSources }: any) {
+      const { bandsIds, genresIds, albumId, artistsIds, ...rest } = await dataSources.trackAPI.updateOne(track);
+      const album = await dataSources.albumAPI.getOne(albumId);
+      const artists = await Promise.all(artistsIds.map((artistId: any) => dataSources.artistAPI.getOne(artistId)));
+      const bands = await Promise.all(bandsIds.map((bandId: any) => dataSources.bandAPI.getOne(bandId)));
+      const genres = await Promise.all(genresIds.map((genreId: any) => dataSources.genreAPI.getOne(genreId)));
+      return { bands, genres, album, artists, ...rest };
+    },
+    async deleteTrack(_parent: any, { id }: any, { dataSources }: any) {
+      const res = await dataSources.trackAPI.deleteOne(id);
+      return res;
+    },
+  },
 };
 
 export default trackResolver;
--- src/models/track/track.schema.ts
+++ src/models/track/track.schema.ts
@@ -4,7 +4,8 @@ const trackSchema = gql`
   type Track {
     id: ID!
     title: String!
-    albums: [Album]
+    album: Album
+    artists: [Artist]
     bands: [Band]
     duration: Int
     released: Int
@@ -15,9 +16,36 @@ const trackSchema = gql`
     items: [Track]
   }
 
+  input ITrack {
+    title: String!
+    albumId: ID
+    bandsIds: [ID]
+    artistsIds: [ID]
+    duration: Int
+    released: Int
+    genresIds: [ID]
+  }
+
+  input IUpdateTrack {
+    id: ID!
+    title: String
+    albumId: ID
+    bandsIds: [ID]
+    artistsIds: [ID]
+    duration: Int
+    released: Int
+    genresIds: [ID]
+  }
+
   type Query {
     track: Track
     tracks: GetTracks
+  }
+
+  type Mutation {
+    createTrack(track: ITrack): Track
+    updateTrack(track: IUpdateTrack): Track
+    deleteTrack(id: ID!): Delete
   }
 `;
 
